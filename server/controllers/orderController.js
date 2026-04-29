1dfe765ce78b572899c905df408213ae738030ad
Ali Mansoori <aliahmedmansoori090@gmail.com>
2026-04-29 22:20:57 +0530

    STRIPE

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -4,7 +4,8 @@ import stripe from "../config/stripe.js";
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = process.env.FRONTEND_URL || "http://localhost:5173";
+  const frontend_url = process.env.FRONTEND_URL;
+  
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
@@ -47,8 +48,8 @@ const placeOrder = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       line_items: line_items,
       mode: "payment",
-      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+      success_url: `${frontend_url}verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}verify?success=false&orderId=${newOrder._id}`,
     });
 
     res.json({ success: true, session_url: session.url });
