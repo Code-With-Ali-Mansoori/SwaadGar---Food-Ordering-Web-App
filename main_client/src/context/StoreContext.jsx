7d5f75a02b54d774d6f3b9f1523a92df322f0c41
Ali Mansoori <aliahmedmansoori090@gmail.com>
2026-04-29 22:10:01 +0530

    sdfg

--- main_client/src/context/StoreContext.jsx
+++ main_client/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ import StoreContext from "../context/create_storeContex";
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
-  const url = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000"; // Use environment variable
+  const url = import.meta.env.VITE_BACKEND_URL ; // Use environment variable
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
